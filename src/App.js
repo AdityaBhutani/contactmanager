1220712e9f5389b1e6a08359e7a2ebc5501d0e0f
Aditya Bhutani <adi_bhutani16@yahoo.in>
2019-06-28 02:28:52 +0530

    Adding Components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>Main Component</h1>
+      <Header branding="Contact manager" />
+      <Contact name="John Doe" email="joe@gmail.com" phone="555-555-5555" />
+      <Contact
+        name="Aditya Bhutani"
+        email="github.com/AdityaBhutani"
+        phone="9999000099"
+      />
     </div>
   );
 }
