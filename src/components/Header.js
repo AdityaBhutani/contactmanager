398df2c2413800d87ceaee97e3556e744fcee67b
Aditya Bhutani <adi_bhutani16@yahoo.in>
2019-06-28 02:31:05 +0530

    PropTypes for props data type validation and setting default props for a component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => {
   const { branding } = props;
@@ -11,6 +12,10 @@ const Header = props => {
 
 Header.defaultProps = {
   branding: "My App"
+};
+
+Header.prototype = {
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;
